fe741f01211091a6198a08462c5502902a49a097
Derek Li <derekli.3628@gmail.com>
2018-02-22 13:16:46 -0500

    removed eb config files

--- derekl/mysite/views.py
+++ derekl/mysite/views.py
@@ -4,6 +4,6 @@ import random
 # Create your views here.
 
 def index(req):
-	greetings = ["Wazzap", "Hello MTV, welcome to my crib!", "ようこそ！", "Nice to meet 'cha", "Welcome", "はじめまして!"]
+	greetings = ["Hello MTV, welcome to my crib!", "ようこそ！", "Nice to meet 'cha", "Welcome", "Error 404 Not Found                                                                                                       jk take a look around!"]
 
 	return render(req, 'index.html', context={"greeting": random.choice(greetings)})
